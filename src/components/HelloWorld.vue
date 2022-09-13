fa2887a19acdc44e6b0c6e85a7b54a183ec159f9
kalyan <kalyan@ngpwebsmart.com>
2022-09-13 14:31:44 +0530

    First Commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,144 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-app>
+    <v-app-bar app height="50px">
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-dialog v-model="dialog" max-width="600px">
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn class="mb-2" small @click="press()" v-bind="attrs" v-on="on" fab color="indigo">
+              <v-icon small>mdi-plus</v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">User Data</span>
+            </v-card-title>
+            <v-card-text>
+              <v-row>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="name" :rules="nameRules" outlined label="Enter your name" required>
+                  </v-text-field>
+                </v-col>
+                <v-col cols="12" sm="6" md="4">
+                  <v-text-field v-model="email" :rules="emailRules" outlined label="Enter your email" required>
+                  </v-text-field>
+                </v-col>
+              </v-row>
+              <v-btn color="success" v-model="submitButton" @click="submit()" v-if="boolValue">
+                Submit
+              </v-btn>
+              <v-btn color="success" @click="edit()" v-else>
+                Update
+              </v-btn>
+            </v-card-text>
+          </v-card>
+        </v-dialog>
+      </v-form>
+    </v-app-bar>
+    <div>
+      <v-simple-table fixed-header>
+        <thead>
+          <tr>
+            <th class="text-left">Name</th>
+            <th class="text-left">Email</th>
+            <th class="text-left">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="row in table" :key="row.name">
+            <td>{{row.name}}</td>
+            <td>{{row.email}}</td>
+            <td>
+              <v-btn fab class="mb-2" small color="cyan" dark @click="update(row)">
+                <v-icon small>mdi-pencil</v-icon>
+              </v-btn>
+              <v-btn fab class="mb-2" @click="deleteData(row.name)" small color="pink">
+                <v-icon small>mdi-delete</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </tbody>
+      </v-simple-table>
+    </div>
+  </v-app>
 </template>
 
 <script>
+import axios from 'axios';
+var instanceOfItem;
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      nameRules: [
+        v => !!v || 'Name is required',
+        v => (v && v.length <= 10) || 'Name must be less than 10 characters',
+      ],
+      emailRules: [
+        v => !!v || 'E-mail is required',
+        v => /.+@.+\..+/.test(v) || 'E-mail must be valid',
+      ],
+      dialog: false,
+      table: undefined,
+      tableData: '',
+      flag: false,
+      boolValue: true,
+      submitButton: true,
+    }
+  },
+  mounted() {
+    axios.get(
+      `http://127.0.0.1:3333/displayData`)
+      .then(response => {
+        // JSON responses are 
+        // automatically parsed
+        this.table = response.data
+      })
+    console.log(this.table)
+
+  },
+  methods: {
+    read() {
+      axios.get(
+        `http://127.0.0.1:3333/displayData`).then(response => {
+          // JSON responses are 
+          // automatically parsed
+          this.table = response.data
+        })
+    },
+    async submit() {
+      await axios.post(`http://127.0.0.1:3333/insertData`, {
+        name: this.name,
+        email: this.email
+      })
+      this.read()
+      this.dialog = false
+    },
+    press() {
+      this.boolValue = true
+    },
+
+    async update(row) {
+      this.boolValue = false;
+      instanceOfItem = row;
+      this.name = row.name
+      this.email = row.email
+      this.dialog = true;
+      this.submitButton = false;
+    },
+
+    async edit() {
+      instanceOfItem.name = this.name
+      instanceOfItem.email = this.email
+      await axios.put(`http://127.0.0.1:3333/updateData/${instanceOfItem.name}`, {
+        name: instanceOfItem.name,
+        email: instanceOfItem.email
+      })
+    },
+    async deleteData(name) {
+      await axios.delete(`http://127.0.0.1:3333/deleteData/${name}`)
+      this.read()
+    },
   }
+
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
